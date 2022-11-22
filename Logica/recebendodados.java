95ed03abd66d88b8d2eaa0e6e522f2131259251b
Orlando Paulo <113864263+Orlando-Paulo99@users.noreply.github.com>
2022-11-22 12:00:22 -0300

    Update recebendodados.java

--- Logica/recebendodados.java
+++ Logica/recebendodados.java
@@ -1,24 +1,35 @@
 package Fundamentos;
 import java.util.Scanner;
 
-public class exercicio01 {
+
+public class exercicio02 {
 
 	public static void main(String[] args) {
 		
-		Scanner entrada=new Scanner(System.in);
-		System.out.println("Digite um numero");
-		int resp=entrada.nextInt();
-		System.out.println(resp);
-		if(resp < 0) {
-			System.out.println("Negativo");
-		}
-		else if (resp==0) {
-			System.out.println("neutro");
-		}
-		else {
-			System.out.println("Positivo");
-		}
-		
-	}
-	
-}
+				Scanner entrada1=new Scanner(System.in);
+				System.out.println("Digite o primerip numero");
+				int resp1=entrada1.nextInt();
+				System.out.println(resp1);
+				
+				Scanner entrada2=new Scanner(System.in);
+				System.out.println("Digite o segundo numero");
+				int resp2=entrada1.nextInt();
+				System.out.println(resp2);
+				
+
+				Scanner entrada3=new Scanner(System.in);
+				System.out.println("Digite o terceiro numero");
+				int resp3=entrada3.nextInt();
+				System.out.println(resp3);
+				
+				if (resp1 > resp2 ) {
+					System.out.println("Maior numero");
+				}
+				
+				else if (resp2 > resp3) {
+					System.out.println("O maior numero");
+				}
+				else if (resp3 > resp1) {
+					System.out.println("O maior numero");
+					
+				}
