936d9d8eda793b815b01cc768b59b7d63cfc3750
Orlando Paulo <113864263+Orlando-Paulo99@users.noreply.github.com>
2022-12-15 11:25:11 -0300

    Create contaTeste.java

---
+++ contaTeste.java
@@ -0,0 +1,16 @@
+public class contaTeste {
+
+	public static void main(String[] args) {
+		contaCorrente c1=new contaCorrente();
+		c1.getVersaldo();
+		c1.setDepositar(2100.0000);
+		System.out.println(c1.getVersaldo());
+		
+		
+		
+		
+		
+		
+	}
+
+}
