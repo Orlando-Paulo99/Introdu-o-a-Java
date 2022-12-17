eb50d125d26233e675845c1f1c49150d89eb9e2d
Orlando Paulo <113864263+Orlando-Paulo99@users.noreply.github.com>
2022-12-17 15:18:06 -0300

    Update contaTeste.java

--- contaTeste.java
+++ contaTeste.java
@@ -1,21 +1,27 @@
+package Banco;
+
+import Banco.contaCorrente;
+
+
 public class contaTeste {
 
   public static void main(String[] args) {
      contaCorrente c1=new contaCorrente();
-     c1.nome1="Orlandk";
+     c1.nome1="Orlando";
      c1.numConta=14668;
-     
+     c1.cpf_1=122377789;
      
      c1.setDepositar(100);
      
             
             
             
-     c1.setSacar(100);
-     System.out.println(c1.getmostrarSaldo());
-            
-            
+     c1.setSacar(10);
      
+    System.out.printf("Nome:%s \nNumer da Conta:%d \nCPF:%d ",c1.nome1,c1.numConta,c1.cpf_1);       
+      System.out.println("\nSeu saldo é: "+c1.getmostrarSaldo());
+            
+    
   }
 
 }
